e5cdd22aa5db652bdc9b8f44b61d0392cf5bbd19
Andrii-Nikitich <nikitich141@gmail.com>
2024-11-29 21:07:27 +0200

    add index file

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,6 @@
-let makTransaction = "";
+function makeTransaction(guantity, pricePerDroid) {
+  console.log(`You ordered ${guantity} droids worth ${pricePerDroid} credits!`);
+}
+makeTransaction(5, 3000);
+makeTransaction(3, 1000);
+makeTransaction(10, 500);
